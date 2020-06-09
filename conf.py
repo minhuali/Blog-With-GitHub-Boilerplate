18fb38c457779bc1ef9545d1eabda6d7c1cb74c9
minhuali <34643500+minhuali@users.noreply.github.com>
2020-06-09 12:03:46 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -9,9 +9,10 @@ build_dir = "../dist/"
 index_page_size = 10
 archives_page_size = 20
 template = {
-    "name": "Galileo",
-    "type": "local",
-    "path": "../Galileo"
+    "name": "Minimal",
+    "type": "git",
+    "url": "https://github.com/pages-themes/minimal",
+    "branch": "latest"
 }
 enable_jsdelivr = {
     "enabled": False,
